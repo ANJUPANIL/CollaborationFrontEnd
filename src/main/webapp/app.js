2158c2eee3060377cb368d90c3c3c14625fdbbcc
ANJUPANIL <anjupanil@20gmail.com>
2016-10-25 05:30:34 -0700

    first

---
+++ src/main/webapp/app.js
@@ -0,0 +1,19 @@
+var app=angular.module('myApp' , ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	
+	.when('/home', {
+		templateUrl : 'c_home/home.html',
+		controller  : 'HomeController'
+	})
+
+	.when('/register', {
+		templateUrl : 'c_user/register.html',
+		controller  : 'UserController'
+	})
+	
+	.otherwise({redirectTo: '/'});
+	alert("hi")
+	
+})
---
+++ src/main/webapp/c_home/HomeController.js
@@ -0,0 +1,4 @@
+
+app.controller('HomeController', function ($scope){
+	$scope.msg = "Home controller loaded";
+})
