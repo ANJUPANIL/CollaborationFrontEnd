c751b43174d4550a64d715b95f893f9f38fb0b6a
ANJUPANIL <anjupanil@20gmail.com>
2016-10-26 05:25:09 -0700

    second

--- src/main/webapp/c_home/HomeController.js
+++ src/main/webapp/c_home/HomeController.js
@@ -1,4 +1,25 @@
+'use strict';
 
-app.controller('HomeController', function ($scope){
-	$scope.msg = "Home controller loaded";
-})
+app.controller('HomeController', ['$scope', 'UserService','$rootScope',
+                                  function($scope, UserService,$rootScope) {
+	
+	console.log("Home Controller..")
+	var self=this;
+	
+	self.getCurrentUser = function()
+	{
+		console.log("Loading current user if already logged in")
+		console.log("Current user :"+$rootScope.currentUser)
+		if(!$rootScope.currentUser){
+			console.log("user not logged in")
+			$rootScope.currentUser="";
+		}
+		return $rootScope.currentUser;
+	}
+	
+	self.getCurrentUser();
+	
+	
+	
+}]);
+
